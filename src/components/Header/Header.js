30cd73a7bd0947875d492467635f89735cba4eae
dojjidi <dojjidi@gmail.com>
2022-01-22 17:30:11 -0700

    css / fonts / style OurSpace Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,9 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md' >
+  <Navbar className='navbar' bg='primary' variant='dark' expand='md' >
     <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }} className='ml-auto'>OurSpace</Link>
+      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }} className='ml-auto logo border-fade'>OurSpace</Link>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
