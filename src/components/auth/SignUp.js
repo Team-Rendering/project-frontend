f55eadc51faebceeaa454a203d4fb42c08e5e178
hishamraihan <mr.hraihan@gmail.com>
2022-01-22 19:50:27 -0500

    added login changes

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -54,54 +54,57 @@ class SignUp extends Component {
     const { email, password, passwordConfirmation } = this.state
 
     return (
-      <div className='row mt-5'>
-        <div className='col-md-6'>
-          <div className='loginLeft'>
-            <h3 className='loginLogo'>OurSpace</h3>
-            <span className='loginDesc'>Connect with friends not around the world yet but soon!</span>
+      <div className='login'>
+
+        <div className='row mt-5'>
+          <div className='col-md-6'>
+            <div className='loginLeft'>
+              <h3 className='loginLogo'>OurSpace</h3>
+              <span className='loginDesc'>Connect with friends not around the world yet but soon!</span>
+            </div>
           </div>
-        </div>
 
-        <div className='col-md-6 mt-5'>
+          <div className='col-md-6 mt-5'>
 
-          <Form className='loginBox' onSubmit={this.onSignUp}>
-            <h3>SignUp</h3>
-            <Form.Group controlId='email'>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                required
-                type='email'
-                name='email'
-                value={email}
-                placeholder='Enter email'
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                required
-                name='password'
-                value={password}
-                type='password'
-                placeholder='Password'
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId='passwordConfirmation'>
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                required
-                name='passwordConfirmation'
-                value={passwordConfirmation}
-                type='password'
-                placeholder='Confirm Password'
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Button className='loginRegisterButton' variant='primary' type='submit'>Submit</Button>
-          </Form>
+            <Form className='loginBox' onSubmit={this.onSignUp}>
+              <h3>SignUp</h3>
+              <Form.Group controlId='email'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  required
+                  type='email'
+                  name='email'
+                  value={email}
+                  placeholder='Enter email'
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  required
+                  name='password'
+                  value={password}
+                  type='password'
+                  placeholder='Password'
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId='passwordConfirmation'>
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control
+                  required
+                  name='passwordConfirmation'
+                  value={passwordConfirmation}
+                  type='password'
+                  placeholder='Confirm Password'
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button className='loginRegisterButton' variant='primary' type='submit'>Submit</Button>
+            </Form>
 
+          </div>
         </div>
       </div>
 
