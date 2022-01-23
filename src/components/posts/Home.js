71dcd74a0fd0ff0b58de53b687e3ca8cbc0e3b20
dojjidi <dojjidi@gmail.com>
2022-01-22 17:30:11 -0700

    css / fonts / style OurSpace Logo

--- src/components/posts/Home.js
+++ src/components/posts/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Home.css'
 // import SignIn from '../auth/SignIn'
 // import { Link } from 'react-router-dom'
 
@@ -6,7 +7,6 @@ import React from 'react'
 
 const Home = () => (
   <>
-    {/* <h4>Lets get ready to make some friends!</h4> */}
     {/* <signUp /> */}
     {/* <Link to='/sign-up'> Sign Up</Link>
     <Link to='/sign-in'> Sign In</Link> */}
