7edd3e4a5fe73518e33ab63adf8d92e2bae32aed
hishamraihan <mr.hraihan@gmail.com>
2022-01-24 11:41:32 -0500

    fixed component

--- src/components/posts/Home.js
+++ src/components/posts/Home.js
@@ -6,14 +6,20 @@ import './Home.css'
 // import SignUp from '../auth/SignUp'
 
 const Home = () => (
+
   <>
-    <h1>Where Your Space becomes My Space, than Our Space
-    </h1>
+
+    <h1>Where Your Space </h1>
+
+    <h1>Becomes My Space </h1>
+
+    <h1>Then Meets OurSpace </h1>
+
     {/* <signUp /> */}
     {/* <Link to='/sign-up'> Sign Up</Link>
     <Link to='/sign-in'> Sign In</Link> */}
-
   </>
+
 )
 
 export default Home
